3407d160337772ef5ec34d08708dd5dbeb1e53d7
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-02 15:34:32 -0800

    Add log messages for device/pipleline connect

--- ashish/index.js
+++ ashish/index.js
@@ -3,8 +3,20 @@
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
 
-	controller.on( 'connect', function() {
-		console.log( 'Successfully connected.');
-	});
+	controller.on( 'connect', onControllerConnect );
+	controller.on('deviceConnected', onDeviceConnect);
+	controller.on('deviceDisconnected', onDeviceDisconnect);
 	
+	function onControllerConnect() {
+		console.log('Successfully connected.');
+	}
+
+	function onDeviceConnect() {
+		console.log('A leap device has been connected');
+	}
+
+	function onDeviceDisconnect() {
+		console.log('A leap device has been connected');
+	}
+
 })();
