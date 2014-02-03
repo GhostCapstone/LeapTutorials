6cff906c4ee175cd30cf93216f5fb761850ec3f3
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-02 21:30:12 -0800

    Complete gettingFrames leap tutorial

--- ashish/gettingFrames/index.js
+++ ashish/gettingFrames/index.js
@@ -1,6 +1,8 @@
 (function() {
 	'use strict';
 	var canvas = document.getElementById('canvas');
+	var width = canvas.width;
+	var height = canvas.height;
 	var button = document.getElementById('unicorn-generator');
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
@@ -10,15 +12,19 @@
 		controller.connect();
 	});
 
+	// animation loop
 	controller.on('frame', function(frame) {
+		// clear the old numbers
+		c.clearRect(0, 0, width, height);
+
 		var numberOfFingers = frame.fingers.length;
 
 		// Define the font shape and size
 		c.font = "30px Arial";
-
-		c.fillText(numberOfFingers, 20, 20);
+		c.textAlign = 'center';
+		c.textBaseline = 'middle';
+		c.fillText(numberOfFingers, width / 2, height / 2);
 
 	});
 
-	
 })();
