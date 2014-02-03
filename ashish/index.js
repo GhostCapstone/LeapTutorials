2740ff3eec029b3ae58924519fd629b8db4672c6
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-02 18:49:23 -0800

    Finish creatingConnection

--- ashish/index.js
+++ ashish/index.js
@@ -1,9 +1,11 @@
 (function() {
 	'use strict';
 	var canvas = document.getElementById('canvas');
+	var button = document.getElementById('unicorn-generator');
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
-
+	var img = document.createElement('img');
+    img.src = "http://i.imgur.com/f68W5EC.png";
 	controller.on( 'connect', onControllerConnect );
 	controller.on('deviceConnected', onDeviceConnect);
 	controller.on('deviceDisconnected', onDeviceDisconnect);
@@ -20,4 +22,14 @@
 		console.log('A leap device has been connected');
 	}
 
+	controller.on( 'ready', function() {
+		// ready code
+		c.drawImage(img, 0, 0);
+	});
+
+	button.addEventListener('click', function() {
+		controller.connect();
+	});
+
+	
 })();
