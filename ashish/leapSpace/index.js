3de94cbfcfc82c48ff02f703cc9b9e873e004a82
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-05 22:12:23 -0800

    initialize foundation for leapSpace

--- ashish/leapSpace/index.js
+++ ashish/leapSpace/index.js
@@ -3,6 +3,7 @@
 	var canvas = document.getElementById('canvas');
 	var width = canvas.width;
 	var height = canvas.height;
+	var button = document.getElementById('unicorn-generator');
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
 
@@ -15,6 +16,14 @@
 	controller.on('frame', function(frame) {
 		// clear the old numbers
 		c.clearRect(0, 0, width, height);
+
+		var numberOfFingers = frame.fingers.length;
+
+		// Define the font shape and size
+		c.font = "30px Arial";
+		c.textAlign = 'center';
+		c.textBaseline = 'middle';
+		c.fillText(numberOfFingers, width / 2, height / 2);
 
 	});
 
