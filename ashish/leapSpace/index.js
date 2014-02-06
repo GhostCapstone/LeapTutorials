403b5676bf0e7cb0395221bc49283ac2f0441503
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-06 11:42:28 -0800

    swap changes to wrong folder -- corrected

--- ashish/leapSpace/index.js
+++ ashish/leapSpace/index.js
@@ -3,7 +3,6 @@
 	var canvas = document.getElementById('canvas');
 	var width = canvas.width;
 	var height = canvas.height;
-	var button = document.getElementById('unicorn-generator');
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
 
@@ -17,14 +16,25 @@
 		// clear the old numbers
 		c.clearRect(0, 0, width, height);
 
-		var numberOfFingers = frame.fingers.length;
-
-		// Define the font shape and size
-		c.font = "30px Arial";
-		c.textAlign = 'center';
-		c.textBaseline = 'middle';
-		c.fillText(numberOfFingers, width / 2, height / 2);
-
 	});
 
+	function leapToScene (frame, leapPos) {
+		// convert
+		var iBox = frame.interactionBox;
+		var top = iBox.center[1] + iBox.size[1]/2;
+		var left = iBox.center[0] - iBox.size[0]/2;
+
+		// modify origin to match canvas
+		// and scale to width and height of canvas
+		var x = (leapPos[0] - left) /
+				iBox.size[0] * width;
+		var y = (leapPos[1] - top) /
+				iBox.size[1] * height;
+
+
+		// y needs to be neg to sync with canvas
+		// inverted nature
+		return [x, ,-y];
+	}
+	controller.connect();
 })();
