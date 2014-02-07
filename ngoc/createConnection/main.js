a47f3c666d37052ee0c077e9d76ab339d8f4b33b
Ngoc Do <ngocmdo@gmail.com>
2014-02-06 23:18:45 -0800

    finished create connection tutorial

--- ngoc/createConnection/main.js
+++ ngoc/createConnection/main.js
@@ -1,12 +1,43 @@
-window.onload() = function(){
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContexy('2d');
-	var controller = new Leap.Controller();
+'use strict';
 
-	// connect controller
-	// only happens when controller is connected
-	// not when controller is connected when page loads
-	controller.on( 'connect' , function(){
-      console.log( 'Successfully connected.' );
-    });
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var controller = new Leap.Controller();
+var button = document.getElementById('unicornButton');
+
+window.onload = function() {
+  // connect controller
+  controller.on('connect', onControllerConnect);
+
+  // connect device
+  // ONLY happens when device is connected
+  // not when page loads with device connected
+  controller.on('deviceConnected', onDeviceConnect);
+
+  // disconnect device
+  // same as above
+  controller.on('deviceDisconnected', onDeviceDisconnect);
+
+  controller.on('ready', onReady);
+
+  button.addEventListener('click', onReady);
+}
+
+function onControllerConnect() {
+  console.log('controller successfully connected');
+}
+
+function onDeviceConnect() {
+  console.log('A Leap device has been connected.');
+}
+
+function onDeviceDisconnect() {
+  console.log('A Leap device has been disconnected.');
+}
+
+function onReady() {
+  console.log('Device ready');
+  var img = document.createElement('img');
+  img.src = "http://i.imgur.com/f68W5EC.png";
+  context.drawImage(img, 0, 0);
 }
