c501329e122ecc8b4a167bf755f81d4bfbe3d6c0
unknown <iguest@is-m440m27-22.ischool.uw.edu>
2014-02-07 00:13:17 -0800

    created folder for getting frames tutorial; copied index.html and main.js from creating connection; added jquery

---
+++ ngoc/getFrames/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var context = $('#canvas')[0].getContext('2d');
+var controller = new Leap.Controller();
+var button = $('#unicornButton');
+
+window.onload = function() {
+  // connect controller
+  $(controller).on('connect', onControllerConnect);
+
+  // connect device
+  // ONLY happens when device is connected
+  // not when page loads with device connected
+  $(controller).on('deviceConnected', onDeviceConnect);
+
+  // disconnect device
+  // same as above
+  $(controller).on('deviceDisconnected', onDeviceDisconnect);
+
+  $(controller).on('ready', onReady);
+
+  $(button).click(onReady);
+}
+
+function onControllerConnect() {
+  console.log('controller successfully connected');
+}
+
+function onDeviceConnect() {
+  console.log('A Leap device has been connected.');
+}
+
+function onDeviceDisconnect() {
+  console.log('A Leap device has been disconnected.');
+}
+
+function onReady() {
+  console.log('Device ready');
+  var img = document.createElement('img');
+  img.src = "http://i.imgur.com/f68W5EC.png";
+  context.drawImage(img, 0, 0);
+}
