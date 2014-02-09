3fd7f445aa24fde717419eac7857e121883d6396
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-09 14:58:10 -0800

    Complete leapSpace

--- ashish/leapSpace/index.js
+++ ashish/leapSpace/index.js
@@ -1,64 +1,112 @@
 (function() {
 	'use strict';
+
+	// Globals
 	var canvas = document.getElementById('canvas');
 	var width = canvas.width;
 	var height = canvas.height;
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller();
 
-	// connect the controller on button press	
-	button.addEventListener('click', function() {
-		controller.connect();
-	});
 
 	// animation loop
 	controller.on('frame', function(frame) {
-		// clear the old numbers
-		c.clearRect(0, 0, width, height);
+		c.clearRect(0, 0, width, height); // reset canvas between frames
 
-		// draw fingers and hands
+
+
+		// draw connector, finger, then hand
 		for (var i = 0; i < frame.hands.length; i++) {
 
-			// draw hand
+			// get the hand
 			var hand = frame.hands[i];
+
 			var handPos = leapToScene(frame, hand.palmPosition);
 
-			// pick color for fillStyle
-			c.fillStyle = '#FF5A40'; // const hand color
+			for (var j = 0; j < hand.fingers.length; j++) {
+				// get finger
+				var finger = hand.fingers[j];
+				var fingerPos = leapToScene(frame, finger.tipPosition);
 
-			// Draw circle at handpos
-			c.beginPath();
-
-			// Draw a circle from 0-2π
-			c.arc(handPos[0], handPos[1], 10, 0, Math.PI*2);
-			c.closePath();
-			c.fill();
-			for (var j = 0; i < hand.fingers.length; i++) {
-				var finger = hand.fingers[i];
-
+				drawConnector(handPos, fingerPos);
+				drawFinger(fingerPos);
 			}
+			drawHand(handPos);
 		}
 
 	});
 
+	// draw a connection from hand-finger using the 
+	// global canvas context
+	function drawConnector(handPos, fingerPos) {
+		c.strokeStyle = '#FFA040';
+		c.lineWidth = 3;
+
+		// Draw the connection
+		c.beginPath();
+		c.moveTo(handPos[0], handPos[1]);
+		c.lineTo(fingerPos[0], fingerPos[1]);
+		c.closePath();
+		c.stroke();
+	}
+
+	// draw a finger using the global canvas context
+	function drawFinger(fingerPos) {
+		// set up stroke style
+		c.strokeStyle = '#39AECF';
+		c.lineWidth = 5;
+
+		// Draw a circle
+		c.beginPath();
+		c.arc(fingerPos[0], fingerPos[1], 6, 0, Math.PI*2);
+		c.closePath();
+		c.stroke();
+	}
+
+	// draw a hand using the global canvas context
+	function drawHand(handPos) {
+		// pick color for fillStyle
+		c.fillStyle = '#FF5A40'; // const hand color
+
+		// Draw a circle from 0-2π
+		c.beginPath();
+		c.arc(handPos[0], handPos[1], 10, 0, Math.PI*2);
+		c.closePath();
+		c.fill();
+	}
+	
 	// Convert leap coordinates to 2d canvas coords
 	function leapToScene (frame, leapPos) {
-		// convert
-		var iBox = frame.interactionBox;
-		var top = iBox.center[1] + iBox.size[1]/2;
-		var left = iBox.center[0] - iBox.size[0]/2;
+      // Gets the interaction box of the current frame
+      var iBox = frame.interactionBox;
 
-		// modify origin to match canvas
-		// and scale to width and height of canvas
-		var x = (leapPos[0] - left) /
-				iBox.size[0] * width;
-		var y = (leapPos[1] - top) /
-				iBox.size[1] * height;
+      // Gets the left border and top border of the box
+      // In order to convert the position to the proper
+      // location for the canvas
+      var left = iBox.center[0] - iBox.size[0]/2;
+      var top = iBox.center[1] + iBox.size[1]/2;
 
+      // Takes our leap coordinates, and changes them so
+      // that the origin is in the top left corner 
+      var x = leapPos[0] - left;
+      var y = leapPos[1] - top;
 
-		// y needs to be neg to sync with canvas
-		// inverted nature
-		return [x, ,-y];
+      // Divides the position by the size of the box
+      // so that x and y values will range from 0 to 1
+      // as they lay within the interaction box
+      x /= iBox.size[0];
+      y /= iBox.size[1];
+
+      // Uses the height and width of the canvas to scale
+      // the x and y coordinates in a way that they 
+      // take up the entire canvas
+      x *= width;
+      y *= height;
+
+      // Returns the values, making sure to negate the sign 
+      // of the y coordinate, because the y basis in canvas 
+      // points down instead of up
+      return [ x , -y ];
 	}
 	controller.connect();
 })();
