625bade44f6e788d3eaa0f326ff8d5bb66c2b9e0
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2014-02-09 21:39:13 -0800

    Add keyTaps, begin impl screentaps

--- ashish/gestures/index.js
+++ ashish/gestures/index.js
@@ -1,6 +1,12 @@
 (function() {
 	'use strict';
 
+	// CONSTANTS
+	var KEYTAP_LIFETIME = 3;
+	var KEYTAP_START_SIZE = 15;
+	var SCREENTAP_LIFETIME = 1;
+	var SCREENTAP_START_SIZE = 30;
+
 	// Globals
 	var canvas = document.getElementById('canvas');
 	var width = canvas.width;
@@ -8,6 +14,8 @@
 	var c = canvas.getContext('2d');
 	var controller = new Leap.Controller({enableGestures: true});
 	var frame;
+	var keyTaps = [];
+	var screenTaps = [];
 
 	// animation loop
 	controller.on('frame', function(data) {
@@ -34,6 +42,8 @@
 			}
 
 		}
+		updateKeyTaps();
+		drawKeyTaps();
 	});
 
 	function onCircle(gesture) {
@@ -66,15 +76,99 @@
 
 
 	function onSwipe(gesture) {
+		var startPos = leapToScene(gesture.startPosition);
+		var pos = leapToScene(gesture.position);
 
+		// prepare c
+		c.strokeStyle = '#FFA040';
+		c.lineWidth = 3;
+		c.beginPath();
+
+		c.moveTo(startPos[0], startPos[1]);
+		c.lineTo(pos[0], pos[1]);
+		c.closePath();
+		c.stroke();
 	}
 
 	function onScreenTap(gesture) {
-		
+		var pos = leapToScene(gesture.position);
+		var time = frame.timestamp;
+		screenTaps.push([pos[0], pos[1], time]);
 	}
 
 	function onKeyTap(gesture) {
+		var pos = leapToScene(gesture.position);
+		var time = frame.timestamp;
+		// add tuples of x, y, timestamp
+		keyTaps.push([pos[0], pos[1], time]);
+	}
 
+	function drawScreenTaps() {
+		for (var i = 0; i < screenTaps.length; i++) {
+			var screenTap = screenTaps[i];
+
+			var x = screenTap[0];
+			var y = screenTap[1];
+
+			var age = frame.timestamp - screenTap[2];
+
+			// draw
+			
+		}
+	}
+
+	function drawKeyTaps() {
+		for (var i = 0; i < keyTaps.length; i++) {
+			var keyTap = keyTaps[i];
+			var age = frame.timestamp - keyTap[2];
+			age /= 1000000;
+			var x = keyTap[0];
+			var y = keyTap[1];
+			var completion = age / KEYTAP_LIFETIME;
+			var timeleft = 1 - completion;
+			var opacity = timeleft;
+			var radius = KEYTAP_START_SIZE * timeleft;
+
+			// drawing static circle
+			c.strokeStyle = '#FF2300';
+			c.lineWidth = 3;
+			c.beginPath();
+			c.arc(x, y, KEYTAP_START_SIZE, 0, Math.PI*2);
+			c.closePath();
+			c.stroke();
+
+			// finger circle
+			c.fillStyle = "rgba(256, 33, 0, " + opacity + ")";
+			c.arc(x, y, radious, 0, Math.PI*2);
+			c.closePath();
+			c.fill();
+		}
+	}
+
+	function updateScreenTaps() {
+		for (var i = 0; i < screenTaps.length; i++) {
+			var screenTap = screenTaps[i];
+			var age = frame.timestamp - screenTaps[i][2];
+			age /= 1000000;
+
+			if (age >= SCREENTAP_LIFETIME) {
+				screenTaps.splice(i, 1);
+			}
+		}
+	}
+	
+	// removes/checks for expired keytaps
+	function updateKeyTaps () {
+		for (var i = 0; i < keyTaps.length; i++) {
+			var keyTap = keyTaps[i];
+
+			// get the difference in time
+			var age = frame.timestamp - keyTaps[i][2];
+			age /= 1000000;
+			if (age >= KEYTAP_LIFETIME) {
+				keyTaps.splice(i, 1);
+			}
+		}
 	}
 
 	
